5284d800f0f73ebf52b2c28280cb69b335b70f9e
Luise <lulu.bauer.lb@gmail.com>
2021-09-19 14:30:53 +0200

    ADDED background weather

--- src/script.js
+++ src/script.js
@@ -1,123 +1,125 @@
 function loadPageCity(city) {
-  let apiKey = "e008579254b3fba07df70d1e8db97913";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(changeTemperature);
+    let apiKey = 'e008579254b3fba07df70d1e8db97913';
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(changeTemperature);
 }
 
 function currentDate(timestamp) {
-  let date = new Date(timestamp);
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[date.getDay()];
-  return `${day}, ${hours}:${minutes}`;
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let day = days[date.getDay()];
+    return `${day}, ${hours}:${minutes}`;
 }
 
 function changeTemperature(response) {
-  console.log(response.data);
-  celsiusTemperature = response.data.main.temp;
-  let temperature = document.querySelector("#temperature");
-  temperature.innerHTML = Math.round(celsiusTemperature);
-  let cityHeading = document.querySelector("#city-heading");
-  cityHeading.innerHTML = response.data.name;
-  let skyCondition = document.querySelector("#sky-condition");
-  skyCondition.innerHTML = response.data.weather[0].main;
-  let humidity = document.querySelector("#humidity");
-  humidity.innerHTML = response.data.main.humidity;
-  let windspeed = document.querySelector("#windspeed");
-  windspeed.innerHTML = Math.round(response.data.wind.speed);
-  let date = document.querySelector("#date");
-  date.innerHTML = currentDate(response.data.dt * 1000);
-  let iconElement = document.querySelector("#icon");
-  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-  checkWeather("");
+    console.log(response.data);
+    let temperature = document.querySelector('#temperature');
+    let cityHeading = document.querySelector('#city-heading');
+    let skyCondition = document.querySelector('#sky-condition');
+    let humidity = document.querySelector('#humidity');
+    let windspeed = document.querySelector('#windspeed');
+    let date = document.querySelector('#date');
+    let iconElement = document.querySelector('#icon');
+
+    celsiusTemperature = response.data.main.temp;
+    temperature.innerHTML = Math.round(celsiusTemperature);
+    cityHeading.innerHTML = response.data.name;
+    skyCondition.innerHTML = response.data.weather[0].main;
+    humidity.innerHTML = response.data.main.humidity;
+    windspeed.innerHTML = Math.round(response.data.wind.speed);
+    date.innerHTML = currentDate(response.data.dt * 1000);
+    iconElement.setAttribute('src', `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+
+    checkWeather(response.data.weather[0].main);
 }
 
 function handleSubmit(event) {
-  event.preventDefault();
-  let city = document.querySelector("#city-input").value;
-  loadPageCity(city);
+    event.preventDefault();
+    let city = document.querySelector('#city-input').value;
+    loadPageCity(city);
 }
 
 function getPosition(position) {
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
-  let apiKey = "e008579254b3fba07df70d1e8db97913";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(changeTemperature);
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    let apiKey = 'e008579254b3fba07df70d1e8db97913';
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(changeTemperature);
 }
 
 function startNavigator() {
-  navigator.geolocation.getCurrentPosition(getPosition);
+    navigator.geolocation.getCurrentPosition(getPosition);
 }
 
 function changeToFahrenheit(event) {
-  event.preventDefault();
-  celsiusButton.classList.remove("active");
-  fahrenheitButton.classList.add("active");
-  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-  let temperature = document.querySelector("#temperature");
-  temperature.innerHTML = Math.round(fahrenheitTemperature);
-
+    event.preventDefault();
+    celsiusButton.classList.remove('active');
+    fahrenheitButton.classList.add('active');
+    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+    let temperature = document.querySelector('#temperature');
+    temperature.innerHTML = Math.round(fahrenheitTemperature);
 }
 
-function changeToCelsius(event){
-  event.preventDefault();
-  celsiusButton.classList.add("active");
-  fahrenheitButton.classList.remove("active");
-  let temperature = document.querySelector("#temperature");
-  temperature.innerHTML = Math.round(celsiusTemperature);
+function changeToCelsius(event) {
+    event.preventDefault();
+    celsiusButton.classList.add('active');
+    fahrenheitButton.classList.remove('active');
+    let temperature = document.querySelector('#temperature');
+    temperature.innerHTML = Math.round(celsiusTemperature);
 }
 
 function checkWeather(weather) {
-  rainElement.classList.remove("rain", "rainlightning");
-  bodyElement.classList.remove("clear", "dark", "clouds", "mist");
-  if (weather == "Thunderstorm") {
-    rainElement.classList.add("rainlightning");
-    bodyElement.classList.add("dark");
-  } else if (weather == "Rain" || weather == "Drizzle") {
-    rainElement.classList.add("rain");
-    bodyElement.classList.add("dark");
-  } else if (weather == "Clouds") {
-    bodyElement.classList.add("clouds");
-  } else if (weather == "Clear"){
-    bodyElement.classList.add("clear");
-  } else {
-    bodyElement.classList.add("mist");
-  }
+    rainElement.classList.remove('rain', 'rainlightning');
+    bodyElement.classList.remove('clear', 'dark', 'clouds', 'mist');
+    snowflakesElement.style.visibility = 'hidden';
+    if (weather == 'Thunderstorm') {
+        rainElement.classList.add('rainlightning');
+        bodyElement.classList.add('dark');
+    } else if (weather == 'Rain' || weather == 'Drizzle') {
+        rainElement.classList.add('rain');
+        bodyElement.classList.add('dark');
+    } else if (weather == 'Clouds') {
+        bodyElement.classList.add('clouds');
+    } else if (weather == 'Clear') {
+        bodyElement.classList.add('clear');
+    } else if (weather == 'Snow') {
+        bodyElement.classList.add('mist');
+        snowflakesElement.style.visibility = 'visible';
+    } else {
+        bodyElement.classList.add('mist');
+    }
 }
 
 let celsiusTemperature = null;
 
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", handleSubmit);
+let searchForm = document.querySelector('#search-form');
+searchForm.addEventListener('submit', handleSubmit);
 
-let currentCityButton = document.querySelector("#current-city-button");
-currentCityButton.addEventListener("click", startNavigator);
+let currentCityButton = document.querySelector('#current-city-button');
+currentCityButton.addEventListener('click', startNavigator);
 
-let fahrenheitButton = document.querySelector("#fahrenheit");
-fahrenheitButton.addEventListener("click", changeToFahrenheit);
+let fahrenheitButton = document.querySelector('#fahrenheit');
+fahrenheitButton.addEventListener('click', changeToFahrenheit);
 
-let celsiusButton = document.querySelector("#celsius");
-celsiusButton.addEventListener("click", changeToCelsius);
+let celsiusButton = document.querySelector('#celsius');
+celsiusButton.addEventListener('click', changeToCelsius);
 
-let rainElement = document.querySelector("#rain");
-let lightningElement = document.querySelector("#lightning");
+let rainElement = document.querySelector('#rain');
+let lightningElement = document.querySelector('#lightning');
 
-let bodyElement = document.querySelector("body");
+let bodyElement = document.querySelector('body');
 
-loadPageCity("London");
+let snowflakesElement = document.querySelector('#snowflakes');
+snowflakesElement.style.visibility = 'hidden';
+
+loadPageCity('London');
+
+snowStorm.resume();
